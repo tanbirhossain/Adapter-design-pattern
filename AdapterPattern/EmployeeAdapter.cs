382c9646090bb665223598596360a2ce3def7aae
Mohammed Tanbir Hossain <pasic.tanbir@hotmail.com>
2019-06-29 14:28:31 +0900

    Add project files.

---
+++ AdapterPattern/EmployeeAdapter.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Xml;
+
+namespace AdapterPattern
+{
+    public class EmployeeAdapter : EmployeeManager, IEmployee
+    {
+        public override string GetAllEmployees()
+        {
+            string returnXml = base.GetAllEmployees();
+            Console.WriteLine("------------------:: Before ::------------\n" + returnXml);
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(returnXml);
+            return JsonConvert.SerializeObject(doc, Newtonsoft.Json.Formatting.Indented);
+        }
+    }
+}
---
+++ AdapterPattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace AdapterPattern
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Setup DI
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IEmployee, EmployeeAdapter>()
+                .BuildServiceProvider();
+
+            var emp = serviceProvider.GetService<IEmployee>();
+            string value = emp.GetAllEmployees();
+            Console.WriteLine("------------------:: After ::------------\n" + value);
+            Console.ReadLine();
+
+        }
+    }
+
+
+
+
+}
